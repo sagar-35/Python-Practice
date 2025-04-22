9849f6ebad0c1a9a80e6ca1bb3f9c64a9c979a97
Ahmedul Hoq <113182836+sagar-35@users.noreply.github.com>
2025-04-23 01:37:26 +0600

    Add files via upload

---
+++ PrimeNumber_Checker.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter Any Number: "))
+
+if is_prime(num):
+    print(f"{num} is a Prime Number")
+else:
+    print(f"{num} is NOT a prime Number")
