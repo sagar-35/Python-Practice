9d3e22c4f84da6dd99deda02c167ee95aa3874b9
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-04-27 01:29:51 +0600

    Update Even Function wiht new change

--- Find_even_function.py
+++ Find_even_function.py
@@ -10,3 +10,20 @@ def find_even(numbers):
 data = [2, 3, 4, 6, 5, 10, 11, 13, 12, 22, 34, 88]
 result = find_even(data)
 print("Even Numbers: ", result)
+
+# Function define
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# User theke input newa
+
+number1 = int(input("Enter first Number: "))
+number2 = int(input("Enter second Number: "))
+
+# Function call kore result ana
+
+result = add_numbers(number1, number2)
+print("Sum of Number is: ",result)
+
+
