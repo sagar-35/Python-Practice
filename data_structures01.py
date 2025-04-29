d0d94f9a9c324f5f21faa9cd08e822c3f549d91d
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-04-29 19:05:57 +0600

    Data Structures 01

---
+++ data_structures01.py
@@ -0,0 +1,18 @@
+#data_structures 
+#list 
+fruits = ['apple', 'banana']
+fruits.append("Orange")
+print(fruits)
+
+#numbers_list
+numbers = [1,2,3,4,5,6,7]
+numbers.append(60)
+print(numbers)
+
+#loop_list
+for fruit in fruits:
+    print(fruit)
+
+
+for number in numbers:
+    print(number)
