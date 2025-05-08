5a98d730978910b936d3c3b7cc52d904b19626a8
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-09 03:21:57 +0600

    Update new oop concept

--- oop_01.py
+++ oop_01.py
@@ -30,3 +30,27 @@ print(c2.brand)
 print(c3.year)
 print(f"Brand: {c1.brand} | Model: {c1.model} | Year: {c1.year}")
         
+
+#Show_Details
+
+class Student:
+    def __init__(self, name, age, subjects):
+        self.name = name
+        self.age = age
+        self.subjects = subjects
+
+    def show_details(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print("Subjects: ")
+        for subject in self.subjects:
+            print(f" - {subject}")
+
+m1 = Student("Hasan", 22, ["Python", "Math", "English"])
+m1.show_details()
+
+        
+
+        
+
+
