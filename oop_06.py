c900c6ab3c90baf02b444b8eef52845edc060740
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-19 01:27:42 +0600

    update with person, student, teacher class

--- oop_06.py
+++ oop_06.py
@@ -1,23 +1,36 @@
-class Animal:
-    def __init__(self, name):
+class Person: # Parent Class
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
     
-    def make_sound(self):
-        print("Some generic animal sound")
+    def show_info(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
     
-class Cat(Animal):
-    def make_sound(self):
-        print(f"{self.name}'s sound Meow")
+class Student(Person): # child class: Student
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+    
+    def show_student(self):
+        self.show_info()
+        print(f"Student ID: {self.student_id}")
+    
+class Teacher(Person):
+    def __init__(self, name, age, subject):
+        super().__init__(name, age)
+        self.subject = subject
+    
+    def show_teacher(self):
+        self.show_info()
+        print(f"Teacher's Subject is: {self.subject}")
 
-class Cow(Animal):
-    def make_sound(self):
-        print(f"{self.name}'s sound Moo")
-a1 = Animal("Some Animal")
-a1.make_sound()
+s1 = Student("Sagar", 22, 525)
+s2 = Student("Sam", 23, 524)
+s3 = Student("Rayhan", 25, 504)
+s1.show_student()
+s2.show_info()
 
-
-c1 = Cat("Cat")
-c1.make_sound()
-
-c2 = Cow("Cow")
-c2.make_sound()
+t1 = Teacher("MD Touhid", 35, "Information System Security")
+t1.show_info()
+t1.show_teacher()
