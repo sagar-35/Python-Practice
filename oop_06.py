de9b64885f14a8160ecf500b3c4b9c88de957d3c
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-19 02:05:24 +0600

    inheritance practice

--- oop_06.py
+++ oop_06.py
@@ -1,21 +1,21 @@
-class Employee:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
-
+class Vehicle:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+    
     def show_info(self):
-        print(f"Name: {self.name}")
-        print(f"Salary: {self.salary}")
+        print(f"Brand: {self.brand}")
+        print(f"Year: {self.year}")
     
-class Doctor(Employee):
-    def __init__(self, name, salary, specialization):
-        super().__init__(name, salary)
-        self.speacialization = specialization
+class Car(Vehicle):
+    def __init__(self, brand, year, model):
+        super().__init__(brand, year)
+        self.model = model
     
-    def treat_patient(self):
-        print(f"Dr. {self.name} is now treating a patient")
-    
-d1 = Doctor("Jaman DJ", 90000, "Cardologist")
-d1.show_info()
-print(f"Speacialization: {d1.speacialization}")
-d1.treat_patient()
+    def drive(self):
+        print(f"The {self.brand} {self.model} is now driving")
+
+c1 = Car("Mercedse", 2022, "Benz")
+c1.show_info()
+print(f"Model: {c1.model}")
+c1.drive()    
