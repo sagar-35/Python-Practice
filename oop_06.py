306df6bf21e8464a569cb899b7fb23ef4a9babda
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-19 01:49:40 +0600

    update with new inheritance classes

--- oop_06.py
+++ oop_06.py
@@ -1,36 +1,21 @@
-class Person: # Parent Class
-    def __init__(self, name, age):
+class Employee:
+    def __init__(self, name, salary):
         self.name = name
-        self.age = age
-    
+        self.salary = salary
+
     def show_info(self):
         print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
+        print(f"Salary: {self.salary}")
     
-class Student(Person): # child class: Student
-    def __init__(self, name, age, student_id):
-        super().__init__(name, age)
-        self.student_id = student_id
+class Doctor(Employee):
+    def __init__(self, name, salary, specialization):
+        super().__init__(name, salary)
+        self.speacialization = specialization
     
-    def show_student(self):
-        self.show_info()
-        print(f"Student ID: {self.student_id}")
+    def treat_patient(self):
+        print(f"Dr. {self.name} is now treating a patient")
     
-class Teacher(Person):
-    def __init__(self, name, age, subject):
-        super().__init__(name, age)
-        self.subject = subject
-    
-    def show_teacher(self):
-        self.show_info()
-        print(f"Teacher's Subject is: {self.subject}")
-
-s1 = Student("Sagar", 22, 525)
-s2 = Student("Sam", 23, 524)
-s3 = Student("Rayhan", 25, 504)
-s1.show_student()
-s2.show_info()
-
-t1 = Teacher("MD Touhid", 35, "Information System Security")
-t1.show_info()
-t1.show_teacher()
+d1 = Doctor("Jaman DJ", 90000, "Cardologist")
+d1.show_info()
+print(f"Speacialization: {d1.speacialization}")
+d1.treat_patient()
