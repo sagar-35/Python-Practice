ce93ade4338d684283947641fbf4e439b6905dc8
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-20 02:54:54 +0600

    update with manager and employee

--- oop_06.py
+++ oop_06.py
@@ -1,22 +1,23 @@
-# Doctor Class Inheritance
-class Person:
-    def __init__(self, name, age):
+#Employee & Manager
+class Employee:
+    def __init__(self, name, salary):
         self.name = name
-        self.age = age
+        self.salary = salary
     
     def show_info(self):
         print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-
-class Doctor(Person):
-    def __init__(self, name, age, specialty):
-        super().__init__(name, age)
-        self.specialty = specialty
+        print(f"Salary: {self.salary}")
     
-    def diagnose(self):
-        print(f"Dr. {self.name} is diagnosing a patient as a {self.specialty}")
+class Manager(Employee):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = department
+    
+    def manage(self):
+        print("Manager of the IT department is managing the QA team.")
 
-dr1 = Doctor("Sagar Hoq", 24, "Dentist")
-dr1.show_info()
-print(f"Specialty: {dr1.specialty}")
-dr1.diagnose()
+mng1 = Manager("Sagar Hoq", 50000, "Software Quality Assurance Team")
+
+mng1.show_info()
+print(f"Department: {mng1.department}")
+mng1.manage()
