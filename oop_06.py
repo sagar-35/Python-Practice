e44e00f6b457b32d8166d0f4f4bd5f1ac0031835
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-20 02:16:18 +0600

    update with new practice

--- oop_06.py
+++ oop_06.py
@@ -1,21 +1,22 @@
 class Vehicle:
-    def __init__(self, brand, year):
+    def __init__(self, brand, year): #attributes
         self.brand = brand
         self.year = year
-    
+
     def show_info(self):
-        print(f"Brand: {self.brand}")
-        print(f"Year: {self.year}")
-    
-class Car(Vehicle):
+        print(f"{self.brand} {self.year} engine is ok.")
+
+class Car(Vehicle): #child class
     def __init__(self, brand, year, model):
         super().__init__(brand, year)
         self.model = model
     
     def drive(self):
-        print(f"The {self.brand} {self.model} is now driving")
+        print(f"The {self.brand} {self.model} is now driving.")
 
-c1 = Car("Mercedse", 2022, "Benz")
-c1.show_info()
-print(f"Model: {c1.model}")
-c1.drive()    
+car1 = Car("BMW", 2022, "I8")
+
+car1.show_info()
+print(f"Model: {car1.model}")
+car1.drive()
+            
