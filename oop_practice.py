d6e13f9fb489e147b2f1d495e4aab9e5a8254003
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-22 02:28:55 +0600

    update with iterator stop method

--- oop_practice.py
+++ oop_practice.py
@@ -1,17 +1,16 @@
-class MyNumbers:
+class MyNumber:
     def __iter__(self):
-        self.a = 1
+        self.num = 1
         return self
-    
-    def __next__(self):
-        x = self.a 
-        self.a += 1
-        return x
-    
-obj = MyNumbers()
-myIter = iter(obj)
 
-print(next(obj))
-print(next(obj))
-print(next(obj))
-print(next(obj))
+    def __next__(self):
+        if self.num <= 5: # stop condition
+            x = self.num
+            self.num += 1
+            return x
+        else:
+            raise StopIteration # stop here
+        
+obj = MyNumber()
+for number in obj:
+    print(number)
