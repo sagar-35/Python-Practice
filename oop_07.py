89efe55f65a926a681dce9b383ca14aa8f9c0686
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-23 02:59:55 +0600

    update with inheritance class Polymorphism

--- oop_07.py
+++ oop_07.py
@@ -1,33 +1,27 @@
-class Car: #Different classes with the same method:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-
-    def move(self):
-        print("Drive!")
-    
-class Boat:
+class Vehicle:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
     
     def move(self):
-        print("Sail!")
+        print("Move!!")
 
-class Plane:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+class Car(Vehicle):
+    pass
 
+class Boat(Vehicle):
     def move(self):
-        print("Fly!")
+        print("Sail!!")
 
-car1 = Car("Mercedse", "Benz")
+class Plane(Vehicle):
+    def move(self):
+        print("Fly!!")
+    
+car1 = Car("BMW", "M5")
 boat1 = Boat("Ibiza", "Touring 20")
 plane1 = Plane("Boeing", "747")
 
 for x in (car1, boat1, plane1):
     x.move()
     print(x.brand)
-
     print(x.model)
