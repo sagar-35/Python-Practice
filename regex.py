b069dea900cde7f2aa095c3499f01f150f346b97
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-26 00:39:25 +0600

    update with raw string notation comment

--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
-# Find All Digits
+# Find All Digits 
 txt = "Order 1: 5 apples, Order 2: 10 bananas, Order 3: 15 mangoes"
 
-x = re.findall(r"\d+", txt)
+x = re.findall(r"\d+", txt) # r raw string notation
 print(x)
