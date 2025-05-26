12ec5474b9bb6cd571531497473799da7bcad2d4
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-26 22:04:02 +0600

    update with urls matching

--- regex.py
+++ regex.py
@@ -1,17 +1,18 @@
 import re 
-# Practice Extract All Valid Dates
-txt = """
-My birthday is 21-03-2000. His birthday is 01/04/1998.
-Invalid date: 99-99-9999. We met on 5-6-2023 and again on 07/07/2021.
-Another event: 29-02-2024.
-"""
-# Regex pattern to match dates (DD-MM-YYYY or DD/MM/YYYY or single digit too)
-pattern = r"\b\d{1,2}[-/]\d{1,2}[-/]\d{4}\b"
+# Practice Extract All Url's from text 
 
-#Find All Matches
+txt = """
+Check out my website: https://www.example.com  
+Visit our blog at http://blog.example.org/articles  
+Or follow us on social: www.facebook.com/ourpage  
+Also: https://youtube.com/watch?v=abc123
+Invalid: http//broken.com, example_com
+"""
+
+pattern = r"(https?://[^\s]+|www\.[^\s]+)"
+
 matches = re.findall(pattern, txt)
 
-# Print results
-print("Valid dates found: ")
-for date in matches:
-    print(date)
+for urls in matches:
+    print(urls)
+
