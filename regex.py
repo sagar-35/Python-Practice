22d0012d571ba62fb571e97af7a627d0ccba9b2e
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-26 20:12:28 +0600

    update with phone number matching

--- regex.py
+++ regex.py
@@ -1,5 +1,15 @@
-import re
-# Find All Emails
-txt = "Please contact us at support@example.com or salse@company.org"
-x = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", txt)
-print(x) 
+import re 
+# Bangladeshi Phone Number finding with regex
+txt = """
+Call me at 01712345678 or +8801812345678.
+Office: 8801912345678. Wrong number: 123456.
+Another: 01911-234567 or +880-1934-567890
+"""
+# regex pattern to match Bangladeshi phone numbers
+pattern = r"(?:\+880-?|880-?|0)?1[3-9][0-9]{2}-?[0-9]{6}"
+
+matches = re.findall(pattern, txt)
+
+print("Valid phone numbers found: ")
+for num in matches:
+    print(num)
